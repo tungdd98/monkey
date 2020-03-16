ba174d14ff379d50a1a324440d424d78044bc50d
Đặng Đức Tùng <tungdd98@gmail.com>
2020-03-16 16:16:30 +0700

    update code

--- resources/js/configs/modules/status.js
+++ resources/js/configs/modules/status.js
@@ -1,6 +1,5 @@
 const status = [
-  { title: 'Không hiển thị', class: 'v-status-disable', value: 0 },
   { title: 'Hiển thị', class: 'v-status-active', value: 1 },
-  { title: 'Huỷ bỏ', class: 'v-status-danger', value: 2 }
+  { title: 'Không hiển thị', class: 'v-status-disable', value: 0 },
 ]
 export default status
--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -60,6 +60,9 @@ export default {
           })
         }
       })
+    },
+    test() {
+      console.log('hihi')
     }
   }
 }
