30f7278c6cdc89e58672f97bb4f280fa3024e708
Đặng Đức Tùng <tungdd98@gmail.com>
2020-03-19 19:08:33 +0700

    update code

---
+++ database/migrations/2020_03_19_103926_articles.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Articles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('description')->nullable();
+            $table->string('content')->nullable();
+            $table->tinyInteger('is_hot')->default(0)->nullable();
+            $table->tinyInteger('is_feature')->default(0)->nullable();
+            $table->tinyInteger('status')->default(1)->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('author')->nullable();
+            $table->string('pseudonym')->nullable();
+            $table->string('source')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
